86d3153547d0cd16915616de5624bdf5fafeb2a4
Fook Athena <fookathe@mi-m4.msk.21-school.ru>
2023-10-31 13:13:08 +0300

    Done 1 quest

---
+++ src/sort.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+int readArr(int* arr) {
+    for (int i = 0; i < 10; i++) {
+        if (scanf("%d", &arr[i]) != 1) {
+            return 0;
+        }
+    }
+    if (getchar() != '\n') {
+            return 0;
+    }
+    return 1;
+}
+
+
+void bubbleSort(int* arr) {
+    for (int i = 0; i < 9; i++) {
+        for (int j = 0; j < 9 - i; j++) {
+            if (arr[j] > arr[j + 1]) {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
+
+void printArr(int* arr) {
+    for (int i = 0; i < 10; i++) {
+        if (i == 9) {
+            printf("%d", arr[i]);
+        } else {
+            printf("%d ", arr[i]);
+        }
+    }
+}
+
+int main() {
+    int arr[10];
+    if (readArr(arr)) {
+        bubbleSort(arr);
+        printArr(arr);
+    } else {
+        printf("n/a");
+    }
+}
