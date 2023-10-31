4eddb7e50743cee1f035ebec4d6dc4396372bf8e
Fook Athena <fookathe@mi-m4.msk.21-school.ru>
2023-10-31 14:37:55 +0300

    Done 3 quest

--- src/key9part1.c
+++ src/key9part1.c
@@ -1,57 +1,75 @@
-/*------------------------------------
-	Здравствуй, человек!
-	Чтобы получить ключ 
-	поработай с комментариями.
--------------------------------------*/
-
 #include <stdio.h>
 
-void input (int *buffer, int *length);
-void output (int *buffer, int length);
+int input(int *buffer, int *length);
+void output(int *buffer, int length);
 int sum_numbers(int *buffer, int length);
-int find_numbers(int* buffer, int length, int number, int* numbers)
+int find_numbers(int *buffer, int length, int number, int *numbers);
 
-/*------------------------------------
-	Функция получает массив данных 
-	через stdin.
-	Выдает в stdout особую сумму
-	и сформированный массив 
-	специальных элементов
-	(выбранных с помощью найденной суммы):
-	это и будет частью ключа
--------------------------------------*/
-int main()
-{
-    
+int main() {
+    int buffer[10], length;
+    if (!input(buffer, &length)) {
+        printf("n/a");
+        return 0;
+    }
+    output(buffer, length);
+    return 0;
 }
 
-/*------------------------------------
-	Функция должна находить
-	сумму четных элементов 
-	с 0-й позиции.
--------------------------------------*/
-int sum_numbers(int *buffer, int length)
-{
-	int sum = 0;
-	
-	for (int i = 1; i < length; i++)
-	{
-		if (i % 2 != 0)
-		{
-			sum = sum + buffer[i];
-		}
-	}
-	
-	return sum;
+int input(int *buffer, int *length) {
+    float len;
+    if (scanf("%f", &len) > 1 || len != (float)((int)len) || len > 10 || len <= 0) {
+        return 0;
+    };
+    *length = (int)len;
+    for (int i = 0; i < (int)len; i++) {
+        if (scanf("%d", &buffer[i]) != 1) {
+            return 0;
+        }
+    }
+    if (getchar() != '\n') {
+        return 0;
+    }
+    return 1;
 }
 
-/*------------------------------------
-	Функция должна находить
-	все элементы, на которые нацело
-	делится переданное число и
-	записывает их в выходной массив.
--------------------------------------*/
-int find_numbers(int* buffer, int length, int number, int* numbers)
-{
+void output(int *buffer, int length) {
+    int number = sum_numbers(buffer, length), numbers[10],
+        newLength = find_numbers(buffer, length, number, numbers);
+    if (newLength) {
+        printf("%d\n", number);
+        for (int i = 0; i < newLength; i++) {
+            if (i == newLength - 1) {
+                printf("%d", numbers[i]);
+            } else {
+                printf("%d ", numbers[i]);
+            }
+        }
 
+    } else {
+        printf("n/a");
+    }
+}
+
+int sum_numbers(int *buffer, int length) {
+    int sum = 0;
+    for (int i = 0; i < length; i++) {
+        if (buffer[i] % 2 == 0) {
+            sum += buffer[i];
+        }
+    }
+    return sum;
+}
+
+int find_numbers(int *buffer, int length, int number, int *numbers) {
+    int index = 0;
+    if (number == 0) {
+        return 0;
+    }
+    for (int i = 0; i < length; i++) {
+        if (buffer[i] != 0 && number % buffer[i] == 0) {
+            numbers[index] = buffer[i];
+            index++;
+        }
+    }
+    return index;
 }
