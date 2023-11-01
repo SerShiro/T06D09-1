19caf7684e1a83de5ff6e0484df6372e9ea2fbb4
Fook Athena <fookathe@mi-l3.msk.21-school.ru>
2023-11-01 15:22:17 +0300

    Done 5 quest

--- src/key9part2.c
+++ src/key9part2.c
@@ -2,25 +2,127 @@
 
 #define LEN 100
 
-void sum(int *buff1, int len1, int *buff2, int len2, int *result, int *result_length);
-void sub(int *buff1, int len1, int *buff2, int len2, int *result, int *result_length);
+int input(int *buff);
+void sum(int *buff1, int len1, int *buff2, int len2, int *result, int *resultLength);
+void sub(int *buff1, int len1, int *buff2, int len2, int *result, int *resultLength);
+void outputSum(int *resultSum, int resultLengthSum);
+void outputSub(int *resultSum, int resultLengthSum);
 
-/*
-    Беззнаковая целочисленная длинная арифметика
-    с использованием массивов.
-    Ввод: 
-     * 2 длинных числа в виде массивов до 100 элементов
-     * В один элемент массива нельзя вводить число > 9
-    Вывод: 
-     * Результат сложения и разности чисел-массивов
-    Пример:
-     * 1 9 4 4 6 7 4 4 0 7 3 7 0 9 5 5 1 6 1
-       2 9
+int main() {
+    int buff1[LEN], buff2[LEN], len1 = input(buff1), len2 = input(buff2), resultSum[LEN], resultSub[LEN],
+                                resultLengthSum, resultLengthSub;
+    if (!(len1 && len2)) {
+        printf("n/a");
+        return 0;
+    }
+    sum(buff1, len1, buff2, len2, resultSum, &resultLengthSum);
+    sub(buff1, len1, buff2, len2, resultSub, &resultLengthSub);
+}
 
-       1 9 4 4 6 7 4 4 0 7 3 7 0 9 5 5 1 9 0
-       1 9 4 4 6 7 4 4 0 7 3 7 0 9 5 5 1 3 2
-*/
-int main()
-{
-    
+int input(int *buffer) {
+    int i = 0;
+    float num;
+    while (i < LEN) {
+        if (scanf("%f", &num) == 1 && num >= 0 && num <= 9 && num == (float)((int)num)) {
+            buffer[i] = (int)num;
+            i++;
+            if (getchar() == '\n') {
+                break;
+            }
+        } else {
+            return 0;
+        }
+    }
+    return i;
+}
+
+void sum(int *buff1, int len1, int *buff2, int len2, int *result, int *resultLength) {
+    int shift = 0;
+    int i, j;
+    if (len1 >= len2) {
+        i = len1 - 1;
+        j = len2 - 1;
+    } else {
+        i = len2 - 1;
+        j = len1 - 1;
+    }
+    while (i >= 0) {
+        if (j < 0) {
+            result[i + 1] = buff1[i] + shift;
+        } else {
+            result[i + 1] = buff1[i] + buff2[j] + shift;
+        }
+        shift = 0;
+        if (result[i + 1] > 9) {
+            shift = 1;
+            result[i + 1] %= 10;
+        }
+        i--;
+        j--;
+    }
+    if (shift != 0) {
+        result[i + 1] = shift;
+    }
+    *resultLength = len1 + 1;
+    outputSum(result, *resultLength);
+}
+
+void outputSum(int *resultSum, int resultLengthSum) {
+    int zeroInStart = 1;
+    for (int i = 0; i < resultLengthSum; i++) {
+        if (zeroInStart && resultSum[i] == 0) {
+            continue;
+        } else {
+            zeroInStart = 0;
+        }
+        printf("%d ", resultSum[i]);
+    }
+    printf("\n");
+}
+
+void sub(int *buff1, int len1, int *buff2, int len2, int *result, int *resultLength) {
+    int shift = 0;
+    int i, j;
+    if (len1 >= len2) {
+        i = len1 - 1;
+        j = len2 - 1;
+        while (i >= 0) {
+            if (j < 0) {
+                result[i] = buff1[i] - shift;
+            } else {
+                result[i] = buff1[i] - buff2[j] - shift;
+            }
+            shift = 0;
+            if (result[i] < 0) {
+                shift = 1;
+                result[i] += 10;
+            }
+            i--;
+            j--;
+        }
+        *resultLength = len1;
+        if (shift != 0) {
+            printf("n/a");
+        } else {
+            outputSub(result, *resultLength);
+        }
+    } else {
+        printf("n/a");
+    }
+}
+
+void outputSub(int *resultSum, int resultLengthSum) {
+    int zeroInStart = 1;
+    for (int i = 0; i < resultLengthSum; i++) {
+        if (zeroInStart && resultSum[i] == 0) {
+            continue;
+        } else {
+            zeroInStart = 0;
+        }
+        if (i == resultLengthSum - 1) {
+            printf("%d", resultSum[i]);
+        } else {
+            printf("%d ", resultSum[i]);
+        }
+    }
 }
